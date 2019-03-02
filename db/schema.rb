c0b03288edf3c8c1fe0f2f402dac830682eea908
Robert Jenkins <bobert@gmail.com>
2019-03-02 18:12:25 -0500

    updated schema and sqlite db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_201158) do
+ActiveRecord::Schema.define(version: 2019_03_02_254956) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_03_02_201158) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "admin_level", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
