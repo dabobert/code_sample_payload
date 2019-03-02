7583b54bb62fcbc77de725057a16783abdd7ebe3
Robert Jenkins <bobert@gmail.com>
2019-03-02 18:04:56 -0500

    user model now requires presence of first_name, last_name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  validates_presence_of :first_name, :last_name, :email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
