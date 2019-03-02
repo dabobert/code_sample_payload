23ff5a1d22cff5ffda8c99df5bd753f9569aaef8
Robert Jenkins <bobert@gmail.com>
2019-03-02 18:46:49 -0500

    got login and top nav workign

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
 
   def is_admin?
