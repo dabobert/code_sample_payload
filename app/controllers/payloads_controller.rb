e25296901c1aca8d890172e009fe9414288380e4
Robert Jenkins <bobert@gmail.com>
2019-03-03 02:03:41 -0500

    the app now only uses uuid with payloads.  ids from table are treated as bad values

--- app/controllers/payloads_controller.rb
+++ app/controllers/payloads_controller.rb
@@ -13,7 +13,7 @@ class PayloadsController < ApplicationController
     begin
       @payload.update_attributes! payload_params
       flash[:success] = "payload Created"
-      redirect_to payload_path(@payload)
+      redirect_to payload_path(@payload.uuid)
     rescue Exception => error
       flash[:error] = error.message
       render :action => :edit
@@ -30,7 +30,7 @@ class PayloadsController < ApplicationController
 
 
   def seek_payload
-    @payload = Payload.seek(params[:id])
+    @payload = Payload.find_by_uuid!(params[:id])
   end
 
   def payload_params
