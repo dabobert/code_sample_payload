5e05a678910dda6cbda91c2ef0b8230032c345a5
Robert Jenkins <bobert@gmail.com>
2019-03-02 20:17:45 -0500

    Users can be created and updated

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,9 +7,54 @@ class Admin::UsersController < AdminController
 
   def new
     @user = User.new
+    render :action => :action
   end
 
+
   def create
+    @user  = User.new user_params
+    begin
+      @user.save!
+      flash[:success] = "New User Created"
+      redirect_to admin_users_path
+    rescue Exception => error
+      flash[:error] = error.message
+      render :action => :action
+    end
   end
+
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to edit_admin_user_path(@user)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :action => :action
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    begin
+      @user.update_attributes! user_params
+      flash[:success] = "User Created"
+      redirect_to admin_users_path
+    rescue Exception => error
+      flash[:error] = error.message
+      render :action => :action
+    end
+  end
+
+  ############################################################################
+  private
+  ############################################################################
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :admin_level, :password, :password_confirmation)
+  end
+
+
 
 end
