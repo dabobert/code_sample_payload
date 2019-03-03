038873fc42c0720604b02af654d8202dcd04daff
Robert Jenkins <bobert@gmail.com>
2019-03-02 21:51:02 -0500

    /admin/payloads can now be listed and created

--- app/controllers/admin/payloads_controller.rb
+++ app/controllers/admin/payloads_controller.rb
@@ -1,5 +1,35 @@
 class Admin::PayloadsController < AdminController
 
   def index
+    @payloads = Payload.paginate(:page => params[:page])
   end
+
+  def new
+    @payload = Payload.new
+    render :action => :action
+  end
+
+  def create
+    @payload  = Payload.new payload_params
+    begin
+      @payload.save!
+      flash[:success] = "New Payload Created"
+      redirect_to admin_payloads_path
+    rescue Exception => error
+      flash[:error] = error.message
+      render :action => :action
+    end
+  end
+
+
+  ############################################################################
+  private
+  ############################################################################
+
+  def payload_params
+    params.require(:payload).permit(:owner_email)
+  end
+
+
+
 end
