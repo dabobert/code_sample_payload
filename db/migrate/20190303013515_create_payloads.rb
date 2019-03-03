b592f087209e16533eaf9d6eed841272c1d1f0e8
Robert Jenkins <bobert@gmail.com>
2019-03-02 20:45:55 -0500

    added owner_email and user_id to payload table. also now indexing uuid

--- db/migrate/20190303013515_create_payloads.rb
+++ db/migrate/20190303013515_create_payloads.rb
@@ -1,12 +1,14 @@
 class CreatePayloads < ActiveRecord::Migration[5.2]
   def change
     create_table :payloads do |t|
-      t.string :uuid
+      t.string :uuid, null: false, index: true
       t.string :file
       t.string :mime
+      t.string :owner_email, null: false
       t.integer :filesize
       t.text :descr
       t.datetime :date_uploaded
+      t.references :user, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_254956) do
+ActiveRecord::Schema.define(version: 2019_03_03_013515) do
+
+  create_table "payloads", force: :cascade do |t|
+    t.string "uuid", null: false
+    t.string "file"
+    t.string "mime"
+    t.string "owner_email", null: false
+    t.integer "filesize"
+    t.text "descr"
+    t.datetime "date_uploaded"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payloads_on_user_id"
+    t.index ["uuid"], name: "index_payloads_on_uuid"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
