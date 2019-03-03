c5714153c46a3fe78658d8ca0ab0891b17bf64ea
Robert Jenkins <bobert@gmail.com>
2019-03-02 21:39:59 -0500

    Added deep funcationality to Payload, so it auto genrates its own uuid
    - created Payload.seek which can find a payload via id or uuid
    - created Payload#deliver which sends an email to the user about the payload
    - created Payload#generate_uuid which generates a unique uuid for a payload

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,3 +1,35 @@
 class Payload < ApplicationRecord
   validates_presence_of :uuid, :owner_email
+
+  after_initialize :generate_uuid
+  after_create :deliver
+
+  def deliver
+    PayloadMailer.with(:payload => self).notification.deliver_now
+  end
+
+
+  class << self
+    def seek(id, raise_error = true)
+      raise(ActiveRecord::RecordNotFound, "Cannot find/seek/search without an id (or uuid)") if raise_error && id.blank?
+
+      if id.to_s.is_i?
+        self.find(id)
+      else
+        self.where(:uuid => id).first
+      end
+    end
+  end
+
+  ############################################################################
+  protected
+  ############################################################################
+
+  def generate_uuid
+    self.uuid ||= loop do
+      random_uuid = SecureRandom.uuid.gsub("-","").slice(0,24)
+      break random_uuid unless self.class.exists?(uuid: random_uuid)
+    end
+  end
+
 end
