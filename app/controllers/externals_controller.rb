f03910635c57efab04b9fecb6480c76f3713c859
Robert Jenkins <bobert@gmail.com>
2019-03-02 20:14:46 -0500

    External Controller now redirects logged in users to the proper section of the app

--- app/controllers/externals_controller.rb
+++ app/controllers/externals_controller.rb
@@ -1,2 +1,8 @@
 class ExternalsController < ApplicationController
+  before_action :check_signed_in
+
+  def check_signed_in
+    redirect_to after_sign_in_path_for(current_user) if signed_in?
+  end
+
 end
